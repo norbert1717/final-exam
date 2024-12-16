f74b038bdbe16cf88c3b7f7e3849214a270396ff
norbert1717 <miklosnorbert17@gmail.com>
2024-12-16 18:31:00 +0100

    finished with jadentalk and carrenting

--- carRenting.js
+++ carRenting.js
@@ -1,5 +1,31 @@
-function carRenting(cars, distance) {
-  //write your code here
-}
+const data = [
+  {
+    type: "Toyota Corolla",
+    avgConsumption: 8.8,
+    fuelAmount: 32
+  },
+  {
+    type: "Skoda Octavia",
+    avgConsumption: 7.2,
+    fuelAmount: 25
+  },
+  {
+    type: "Fiat Uno",
+    avgConsumption: 6.1,
+    fuelAmount: 24
+  }
+]
 
+function carRenting(cars, distance) {
+  const goodCars = [];
+  for (const car of cars) {
+    const maxDistance = (car.fuelAmount / car.avgConsumption) * 100;
+    if (maxDistance >= distance) {
+      goodCars.push(car.type);
+    }
+  }
+  return goodCars.length > 0 ? goodCars : null;
+
+}
+  
 module.exports = carRenting
--- jadenTalk.js
+++ jadenTalk.js
@@ -1,5 +1,8 @@
 function jadenTalk(text) {
-  //write your code here
+
+  const result = text.replace(/[.,\/#!$%\^&\*;:{}=\-_~()]/g, "")
+  return result.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
 }
 
+console.log(jadenTalk("How can mirrors be real, if our eyes aren't real?"))
 module.exports = jadenTalk
